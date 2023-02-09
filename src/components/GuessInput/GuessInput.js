ee7b60b68fda38efd86575c1bf4f10c1571f567b
payapula <bharathikannank05@gmail.com>
2023-02-09 20:50:27 +0530

    - Ex 1 completed

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (guess.length !== 5) {
+          return alert("Min length should be 5");
+        }
+        setGuess("");
+        console.log(guess);
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        type="text"
+        id="guess-input"
+        value={guess}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        minLength={5}
+        maxLength={5}
+        onChange={(e) => {
+          setGuess(e.target.value.toLocaleUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
