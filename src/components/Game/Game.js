ffbfa57f1dd0b580b2310bbc5e47b89ce7e67cc7
payapula <bharathikannank05@gmail.com>
2023-02-16 11:49:20 +0530

    - Ex 4 completed

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import { sample } from "../../utils";
-import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
 import Guesses from "../Guesses";
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+// const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+// console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../..//game-helpers";
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+
+const answer = sample(WORDS);
+console.log("answer", answer);
 
 function Guess({ guess = "" }) {
+  const result = checkGuess(guess, answer);
   return (
     <p className="guess">
-      {range(5).map((num) => (
-        <span key={num} className="cell">
-          {guess[num]}
-        </span>
-      ))}
+      {range(5).map((num) => {
+        const guessResult = result?.[num];
+        const statusClass = guessResult?.status ?? "";
+        return (
+          <span key={num} className={`cell ${statusClass}`}>
+            {guess[num]}
+          </span>
+        );
+      })}
     </p>
   );
 }
